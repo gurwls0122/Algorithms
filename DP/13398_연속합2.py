5c9306bec2ae61c2f0737906894d809a5494f839
gurwls0122 <80228712+gurwls0122@users.noreply.github.com>
2023-08-16 21:55:33 +0900

    BOJ/dp/13398 연속합2

---
+++ DP/13398_연속합2.py
@@ -0,0 +1,23 @@
+import sys
+n = int(sys.stdin.readline())
+lst = list(map(int, sys.stdin.readline().split()))
+# 제거하지 않은 연속합을 저장하기 위한 배열
+dp = []
+
+# 한개의 값이 제거된 연속합을 저장하기 위한 배열
+dp2= []
+
+for i in range(n):
+    if i==0:
+        dp.append(lst[i])
+        dp2.append(lst[i])
+    elif i==1:
+        dp.append(max(lst[i], dp[i-1] + lst[i]))
+        dp2.append(max(dp[i-1], lst[i]))
+    else:
+        dp.append(max(lst[i], dp[i-1]+ lst[i]))
+        dp2.append(max(dp2[i-1]+lst[i], dp[i-1]))
+
+# print(dp)
+# print(dp2)
+print(max(max(dp), max(dp2)))
